7ce5d2c5cdc71936de54147f05b1f5f17dd9f9c6
Lee Myeonghwan <leemyeonghwan@Gobans-MacBook.local>
2022-05-25 20:53:45 +0900

    reslove #3

--- Caculator_G/Caculator_G/ViewController.swift
+++ Caculator_G/Caculator_G/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .black
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchNumber(_ sender: UIButton) {
+        let num = sender.titleLabel?.text
+        print("touched \(num ?? "error")")
+    }
+    
 }
 
